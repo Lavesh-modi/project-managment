cc8ac36ec3cc951744ad29a52ad98cd164eac47d
Your Name <laveshmodiag@gmail.com>
2025-05-06 19:53:28 +0530

    new feature

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { ThemeProvider } from './context/ThemeContext';
 import Dashboard from './pages/Dashboard';
 import ProjectDetails from './pages/ProjectDetails';
 import Settings from './pages/Settings';
 import Layout from './components/Layout';
+import ThemeToggle from './components/ThemeToggle';
+import './styles/themes.css';
 import './App.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/project/:id" element={<ProjectDetails />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </Layout>
-      </Router>
+      <ThemeProvider>
+        <Router>
+          <Layout>
+            <ThemeToggle />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/project/:id" element={<ProjectDetails />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </ThemeProvider>
     </Provider>
   );
 }
